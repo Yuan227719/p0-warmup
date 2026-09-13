28b1df68f47262d91b395a00e9f6784fb196ab0c
Yuan227719 <yuant2004@163.com>
2026-09-13 23:22:16 +0800

    W1 final: exercise 6.5 convert with raise/catch pattern

--- temperature_transfer.py
+++ temperature_transfer.py
@@ -1,25 +1,34 @@
-def to_fahrenheit(celsius, precision=1):
-
-    fah = celsius*9/5+32
-
-    return f"{fah:.{precision}f}"
 
 
+def convert(temp):
+    if temp[-1].upper() == 'C':
+        try:
+            value = float(temp[:-1])
+        except ValueError:
+            raise ValueError(f"无法解析: {temp}")
+        fah = value*9/5+32
+        return fah
+    elif temp[-1].upper() == 'F':
+        try:
+            value = float(temp[:-1])
+        except ValueError:
+            raise ValueError(f"无法解析: {temp}")
+        cel = (value-32)*5/9
+        return cel
+    else:
+        raise ValueError(f"无法解析: {temp}")
 
-
-def to_celsius(fahrenheit, precision=1):
-
-    cel = (fahrenheit-32)*5/9
-    
-    return f"{cel:.{precision}f}"
 
 
 
 
 if __name__ == "__main__":
-    print(to_fahrenheit(25))              # 77.0
-    print(to_fahrenheit(25, precision=2)) # 77.00
-    print(to_fahrenheit(37))              # 98.6
-    print(to_celsius(98.6))               # 37.0
-    print(to_celsius(100, precision=0))   # 38  ← 不是 38.0，注意整数
-
+    try:
+                   # 77.0
+        print(convert('25f')) # 77.00
+        print(convert('abc'))   
+        # print(to_fahrenheit(37))              # 98.6
+        # print(to_celsius('abc'))               # 37.0
+        # print(to_celsius(100, precision=0))   # 38  ← 不是 38.0，注意整数
+    except ValueError as e:
+        print(f'捕获异常: {e}')
